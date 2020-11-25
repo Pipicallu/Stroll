08a7d92d5263ad79ed001f6251b178f3b8b5d276
Pipicallu <thomas.vassallo@gmail.com>
2020-11-25 08:55:19 +0000

    redid navbar with no bootstrap so as to creat custom hamburger menu

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,8 @@
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.getElementsByClassName('.menu-btn');
 let menuOpen = false;
 
-menuBtn.addEventListener('click', () =>{
-    if(!menuBtn){
+menuBtn.addEventListener('click', () => {
+    if(!menuBtn) {
         menuBtn.classList.add('open');
         menuOpen = true;
     } else {
