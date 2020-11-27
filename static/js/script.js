da6c01db9865575a9bb24e0924248cec06db84fd
Pipicallu <thomas.vassallo@gmail.com>
2020-11-27 21:48:19 +0000

    added media query and js for responsive navbar

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,19 @@
-const menuBtn = document.getElementsByClassName('.menu-btn');
+const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
-    if(!menuBtn) {
+    if(!menuOpen) {
         menuBtn.classList.add('open');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
         menuOpen = false;
     }
+});
+
+/* nav bar display */
+
+const navbarLinks = document.getElementsByClassName('NavBar-links')[0]
+menuBtn.addEventListener('click', () =>{
+    navbarLinks.classList.toggle('active');
 });
